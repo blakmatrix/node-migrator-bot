94ee6e90e1795be5206601e5d8a115f4d3f05f91
Farrin Reid <blakmatrix@gmail.com>
2012-06-23 12:52:02 -0800

    [fix] Changed require('util') to require('util') for compatibility with node v0.8

--- test/simple.js
+++ test/simple.js
@@ -1,14 +1,14 @@
 
-var sys = require("sys");
-var sys = require('sys');
+var util = require('util');
+var util = require('util');
 
-var   sys   =     require ( 'sys' );
+var   util = require('util');
 var   sys   =     require ( 's'+'y'+'s' );
 var system = '2';
 var SySTEMBLARG = '2';
-sys.puts("sys should be replaced with util, but not in this output or "+system);
-sys.puts("1 ");
-sys.puts("2");
-sys.puts("3");
+util.puts("sys should be replaced with util, but not in this output or "+system);
+util.puts("1 ");
+util.puts("2");
+util.puts("3");
 
 getNewFile(function (err, file) { if (err) { throw err; } /* do something with file */ });
